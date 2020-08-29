9ee4b77b7834ab9ece3ba3414107fb0f2cc38efe
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-08-29 21:22:20 +0300

    Create user and make leads available to authenticated users only

--- lead_manager/leads/api.py
+++ lead_manager/leads/api.py
@@ -5,8 +5,13 @@ from .serializers import LeadSerializer
 
 
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
     serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
