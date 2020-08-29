1728857293a0f403dadd90bfa0038d4f450b6e80
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-08-29 22:48:48 +0300

    Add logout to invalidate the token

--- lead_manager/accounts/api.py
+++ lead_manager/accounts/api.py
@@ -30,4 +30,13 @@ class LoginAPI(generics.GenericAPIView):
             "token": AuthToken.objects.create(user)[1]
         })
 
-# Get User API
+
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
--- lead_manager/accounts/urls.py
+++ lead_manager/accounts/urls.py
@@ -1,10 +1,12 @@
 from knox import views as knox_views
 from django.urls import path, include
 
-from .api import RegisterAPI, LoginAPI
+from .api import RegisterAPI, LoginAPI, UserAPI
 
 urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/register', RegisterAPI.as_view()),
-    path('api/auth/login', LoginAPI.as_view())
+    path('api/auth/login', LoginAPI.as_view()),
+    path('api/auth/user', UserAPI.as_view()),
+    path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
 ]
