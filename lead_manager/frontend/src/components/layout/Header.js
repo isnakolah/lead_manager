ef4e2ab52a27f4ea8d14d4710522eef2592b108f
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-08-30 12:12:02 +0300

    Show appropriate links based on auth

--- lead_manager/frontend/src/components/layout/Header.js
+++ lead_manager/frontend/src/components/layout/Header.js
@@ -1,7 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-const Header = () => {
+const Header = ({ auth }) => {
+  const { isAuthenticated, user } = auth;
+
+  const authLinks = (
+    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+      <li className="nav-item">
+        <button className="nav-link btn btn-info btn-sm text-light">
+          Logout
+        </button>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+      <li className="nav-item">
+        <Link to="/register" className="nav-link">
+          Register
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/login" className="nav-link">
+          Login
+        </Link>
+      </li>
+    </ul>
+  );
+
   return (
     <nav className="navbar navbar-expand-sm navbar-light bg-light">
       <div className="container">
@@ -20,22 +49,19 @@ const Header = () => {
           <a className="navbar-brand" href="#">
             Lead Manager
           </a>
-          <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li className="nav-item">
-              <Link to="/register" className="nav-link">
-                Register
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/login" className="nav-link">
-                Login
-              </Link>
-            </li>
-          </ul>
         </div>
+        {isAuthenticated ? authLinks : guestLinks}
       </div>
     </nav>
   );
 };
 
-export default Header;
+Header.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(Header);
