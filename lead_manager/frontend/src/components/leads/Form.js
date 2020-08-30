5d8c839fa4509ff031905cb20d03cd5a6455f5cc
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-08-30 15:09:00 +0300

    Restructure lead form

--- lead_manager/frontend/src/components/leads/Form.js
+++ lead_manager/frontend/src/components/leads/Form.js
@@ -4,20 +4,23 @@ import PropTypes from "prop-types";
 
 import { addLead } from "../../actions/leads";
 
-const Form = props => {
+import Input from "../common/Input";
+
+const Form = ({ addLead }) => {
   const [lead, setLead] = useState({
     name: "",
     email: "",
     message: "",
   });
 
+  const { name, email, message } = lead;
+
   const onChange = ({ target }) =>
     setLead({ ...lead, [target.name]: target.value });
 
   const onSubmit = e => {
     e.preventDefault();
-    const lead_add = { ...lead };
-    props.addLead(lead_add);
+    addLead({ ...lead });
     setLead({
       name: "",
       email: "",
@@ -29,26 +32,8 @@ const Form = props => {
     <div className="card card-body mt-4 mb-4">
       <h2>Add Lead</h2>
       <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            className="form-control"
-            onChange={onChange}
-            value={lead.name}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            name="email"
-            className="form-control"
-            onChange={onChange}
-            value={lead.email}
-          />
-        </div>
+        <Input label="Name" name="name" onChange={onChange} value={name} />
+        <Input label="Email" name="email" onChange={onChange} value={email} />
         <div className="form-group">
           <label htmlFor="message">Message</label>
           <textarea
@@ -56,7 +41,7 @@ const Form = props => {
             name="message"
             className="form-control"
             onChange={onChange}
-            value={lead.message}
+            value={message}
           />
         </div>
         <div className="form-group">
